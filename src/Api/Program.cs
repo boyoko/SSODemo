2441ab02903961b979ef9997efc9c92517d4e684
赵东升 <zhaodongsheng@cabrtech.com>
2022-07-30 19:12:16 +0800

    Add project files.

---
+++ src/Api/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "api1");
+    });
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers()
+        .RequireAuthorization("ApiScope");
+});
+
+//app.MapControllers();
+
+app.Run();
---
+++ src/Identityserver/Config.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Identityserver
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api1", "My Api")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+            {
+                ClientId = "Client",
+
+                //没有交互式的用户，使用客户端/机密 来进行认证
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                //用于加密的密码
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                //定义客户端可以访问的API Scope
+                AllowedScopes = { "api1" }
+            }
+            };
+    }
+}
