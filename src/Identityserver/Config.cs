62c4500f6224c0480d6b7ad29097d4024f84d223
赵东升 <zhaodongsheng@cabrtech.com>
2022-07-30 19:39:01 +0800

    v2 add UI and mvc client

--- src/Identityserver/Config.cs
+++ src/Identityserver/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -12,7 +13,8 @@ namespace Identityserver
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             { 
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -25,21 +27,42 @@ namespace Identityserver
             new Client[] 
             {
                 new Client
-            {
-                ClientId = "Client",
-
-                //没有交互式的用户，使用客户端/机密 来进行认证
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                //用于加密的密码
-                ClientSecrets =
                 {
-                    new Secret("secret".Sha256())
+                    ClientId = "Client",
+
+                    //没有交互式的用户，使用客户端/机密 来进行认证
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    //用于加密的密码
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    //定义客户端可以访问的API Scope
+                    AllowedScopes = { "api1" }
                 },
 
-                //定义客户端可以访问的API Scope
-                AllowedScopes = { "api1" }
-            }
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
             };
     }
 }
